1e25e9dc423335bbb9a6a4f6cbcce547c9d416c6
Thomas Waters <twaters117@gmail.com>
2019-04-14 10:19:39 -0400

    Abstracted Grid Drawing Algorithm

--- penplot-grid.js
+++ penplot-grid.js
@@ -9,40 +9,17 @@ export const orientation = Orientation.LANDSCAPE;
 export const dimensions = PaperSize.LETTER;
 
 export default function createPlot (context, dimensions) {
-  const [ width, height ] = dimensions;
   const margin = 2;
-  const working_width = width - 2*margin;
-  const working_height = height - 2*margin;
   const cell_size = 2;
   const min_padding = 0.25;
 
-  const n_v = Math.floor( (working_height + min_padding) / (cell_size + min_padding) );
-  const n_h = Math.floor( (working_width  + min_padding) / (cell_size + min_padding) );
-
-  const v_padding = (height - (2*margin + n_v*cell_size)) / (n_v - 1);
-  const h_padding = (width  - (2*margin + n_h*cell_size)) / (n_h - 1);
-
-  const padding = Math.min(v_padding, h_padding);
-
-  const lines = flattenLineTree(array(n_v).map((_, y) => {
-    return array(n_h).map((_, x) => {
-      const x_pad = margin + x * h_padding;
-      const y_pad = margin + y * v_padding;
-      const x_min = x_pad + x * cell_size;
-      const x_max = x_pad + (x+1) * cell_size;
-      const y_min = y_pad + y * cell_size;
-      const y_max = y_pad + (y+1) * cell_size;
-      const points = [
-        [x_min, y_min], [x_max, y_min],
-        [x_max, y_max], [x_min, y_max]
-      ];
-
-      points.push(points[0]);
-      return points;
-    });
+  const lines = flattenLineTree(grid({
+    dimensions,
+    margin,
+    cell_size,
+    min_padding,
+    algorithm : box
   }));
-
-  console.log(lines);
 
   return {
     draw,
@@ -70,7 +47,57 @@ export default function createPlot (context, dimensions) {
 
   // ---- Helper Functions -----------------------------------------------------
 
-  function grid() {
+  function box(pos, dimensions) {
+    const [x, y] = pos;
+    const [width, height] = dimensions;
 
+    let points = [
+      [x, y], [x + width, y], [x + width, y + height], [x, y + height]
+    ];
+
+    points.push(points[0]);
+    return points;
+  }
+
+  /**
+   * opts:
+   *  dimensions
+   *  margin
+   *  cell_size
+   *  min_padding
+   *  algorithm
+   */
+  function grid(opts) {
+    const [ width, height ] = opts.dimensions;
+    const working_width     = width  - 2*opts.margin;
+    const working_height    = height - 2*opts.margin;
+
+    const n_v = Math.floor(
+      (working_height + opts.min_padding) / (opts.cell_size + opts.min_padding)
+    );
+    const n_h = Math.floor(
+      (working_width  + opts.min_padding) / (opts.cell_size + opts.min_padding)
+    );
+
+    const v_padding = (height - (2*opts.margin + n_v * opts.cell_size)) / (n_v - 1);
+    const h_padding = (width  - (2*opts.margin + n_h * opts.cell_size)) / (n_h - 1);
+
+    const padding = Math.min(v_padding, h_padding);
+
+    return array(n_v).map((_, y) => {
+      return array(n_h).map((_, x) => {
+        const x_pad = opts.margin + x * h_padding;
+        const y_pad = opts.margin + y * v_padding;
+
+        const pos = [
+          x_pad + x * opts.cell_size,
+          y_pad + y * opts.cell_size
+        ];
+        const cell_dim = [cell_size, cell_size];
+
+        const lines = opts.algorithm(pos, cell_dim);
+        return lines;
+      });
+    });
   }
 }

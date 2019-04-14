41ece0de407581e0c8821d9ff0370517044b3d2e
Thomas Waters <twaters117@gmail.com>
2019-04-14 10:01:53 -0400

    Rectangle Grid Creation

--- penplot-grid.js
+++ penplot-grid.js
@@ -1,37 +1,48 @@
 import { PaperSize, Orientation } from 'penplot';
 import { polylinesToSVG } from 'penplot/util/svg';
 import { clipPolylinesToBox } from 'penplot/util/geom';
+import array from 'new-array';
+import flattenLineTree from 'flatten-line-tree';
+import optimizePaths from 'optimize-paths';
 
 export const orientation = Orientation.LANDSCAPE;
 export const dimensions = PaperSize.LETTER;
 
 export default function createPlot (context, dimensions) {
   const [ width, height ] = dimensions;
-  let lines = [];
+  const margin = 2;
+  const working_width = width - 2*margin;
+  const working_height = height - 2*margin;
+  const cell_size = 2;
+  const min_padding = 0.25;
 
-  // Draw some circles expanding outward
-  const steps = 5;
-  const count = 20;
-  const spacing = 1;
-  const radius = 2;
-  for (let j = 0; j < count; j++) {
-    const r = radius + j * spacing;
-    const circle = [];
-    for (let i = 0; i < steps; i++) {
-      const t = i / Math.max(1, steps - 1);
-      const angle = Math.PI * 2 * t;
-      circle.push([
-        width / 2 + Math.cos(angle) * r,
-        height / 2 + Math.sin(angle) * r
-      ]);
-    }
-    lines.push(circle);
-  }
+  const n_v = Math.floor( (working_height + min_padding) / (cell_size + min_padding) );
+  const n_h = Math.floor( (working_width  + min_padding) / (cell_size + min_padding) );
 
-  // Clip all the lines to a margin
-  const margin = 1.5;
-  const box = [ margin, margin, width - margin, height - margin ];
-  lines = clipPolylinesToBox(lines, box);
+  const v_padding = (height - (2*margin + n_v*cell_size)) / (n_v - 1);
+  const h_padding = (width  - (2*margin + n_h*cell_size)) / (n_h - 1);
+
+  const padding = Math.min(v_padding, h_padding);
+
+  const lines = flattenLineTree(array(n_v).map((_, y) => {
+    return array(n_h).map((_, x) => {
+      const x_pad = margin + x * h_padding;
+      const y_pad = margin + y * v_padding;
+      const x_min = x_pad + x * cell_size;
+      const x_max = x_pad + (x+1) * cell_size;
+      const y_min = y_pad + y * cell_size;
+      const y_max = y_pad + (y+1) * cell_size;
+      const points = [
+        [x_min, y_min], [x_max, y_min],
+        [x_max, y_max], [x_min, y_max]
+      ];
+
+      points.push(points[0]);
+      return points;
+    });
+  }));
+
+  console.log(lines);
 
   return {
     draw,
@@ -41,7 +52,9 @@ export default function createPlot (context, dimensions) {
     clear: true
   };
 
-  function draw () {
+  // ---- Main Functions -------------------------------------------------------
+
+  function draw() {
     lines.forEach(points => {
       context.beginPath();
       points.forEach(p => context.lineTo(p[0], p[1]));
@@ -49,9 +62,15 @@ export default function createPlot (context, dimensions) {
     });
   }
 
-  function print () {
+  function print() {
     return polylinesToSVG(lines, {
       dimensions
     });
+  }
+
+  // ---- Helper Functions -----------------------------------------------------
+
+  function grid() {
+
   }
 }

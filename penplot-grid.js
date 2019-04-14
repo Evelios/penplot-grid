e3511868d9d0f8ebb7dcb059704c3b7b00a47324
Thomas Waters <twaters117@gmail.com>
2019-04-14 14:29:12 -0400

    Grid Filled Crosshatches

--- penplot-grid.js
+++ penplot-grid.js
@@ -1,30 +1,56 @@
 import { PaperSize, Orientation } from 'penplot';
 import { polylinesToSVG } from 'penplot/util/svg';
 import { clipPolylinesToBox } from 'penplot/util/geom';
+import { randomFloat } from 'penplot/util/random';
 import array from 'new-array';
 import flattenLineTree from 'flatten-line-tree';
 import optimizePaths from 'optimize-paths';
+import polygonCrosshatching from 'polygon-crosshatching';
+import SimplexNoise from 'simplex-noise';
 
 export const orientation = Orientation.LANDSCAPE;
 export const dimensions = PaperSize.LETTER;
 
 export default function createPlot (context, dimensions) {
-  const margin = 2;
-  const cell_size = 2;
-  const min_padding = 0.25;
+  const margin        = 2;
+  const cell_size     = 1;
+  const min_padding   = 0.1;
+  const pen_width     = 0.02;
+  const min_spacing   = 1 * pen_width;
+  const simplex       = new SimplexNoise();
+
+  const noise = pos => {
+    const scale = 20;
+    return simplex.noise2D(scale * pos[0], scale * pos[1]);
+  }
+
+  const crosshatch = (pos, dim) => {
+    const redistribute = x => Math.pow(x, 0.5);
+    const noise_ammount = (noise(pos) + 1) / 2;
+    const polygon = box(pos, dim);
+    const angle = noise_ammount * 2*Math.PI;
+    const max_spacing = min_spacing + 20 * min_spacing * noise_ammount;
+    return polygonCrosshatching(
+      polygon,
+      angle,
+      min_spacing,
+      max_spacing,
+      redistribute
+    );
+  }
 
   const lines = flattenLineTree(grid({
     dimensions,
     margin,
     cell_size,
     min_padding,
-    algorithm : box
+    algorithm : crosshatch
   }));
 
   return {
     draw,
     print,
-    background: 'white',
+    background: 'black',
     animate: false,
     clear: true
   };
@@ -32,16 +58,20 @@ export default function createPlot (context, dimensions) {
   // ---- Main Functions -------------------------------------------------------
 
   function draw() {
+    context.strokeStyle = '#FFF';
+
     lines.forEach(points => {
       context.beginPath();
+      context.lineWeight = pen_width;
       points.forEach(p => context.lineTo(p[0], p[1]));
       context.stroke();
     });
   }
 
   function print() {
-    return polylinesToSVG(lines, {
-      dimensions
+    return polylinesToSVG(optimizePaths(lines), {
+      dimensions,
+      lineWeight  : pen_width,
     });
   }
 
@@ -55,7 +85,6 @@ export default function createPlot (context, dimensions) {
       [x, y], [x + width, y], [x + width, y + height], [x, y + height]
     ];
 
-    points.push(points[0]);
     return points;
   }
 
